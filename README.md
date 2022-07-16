0e12b50892f695949fc69aba881181a86d4a030a
Ben <105577567+benjbright@users.noreply.github.com>
2022-07-16 12:15:12 +0100

    Update work on testing

--- README.md
+++ README.md
@@ -4,7 +4,11 @@ Walkthrough of the Hardhat tutorial project
 
 ## Notes and key learning points
 
-### Notes on `Token.js`
+### Notes on Testing contracts
+
+- Seup `Token.js` for testing
+
+Run `npx hardhat test`
 
 ```
     const [owner] = await.ethers.getSigners()
@@ -26,3 +30,23 @@ A `ContractFactory` in ethers.js is an abstraction used to deploy new smart cont
 ```
 
 Calling a `deploy()` method on a `ContractFactory` will start the deployment and return a `Promise` that resolves to a `Contract`. This is the object that has a method for each of your smart contract functions.
+
+```
+const ownerBalance = await hardhatToken.balanceOf(owner.address)
+```
+
+Once the contract is deployed, can call contract methods on `hardhatToken`. Here get the balance of the owner account by calling the contract's `balanceOf` method.
+Note that the account that deploys the contract gets the entire supply, by default `ContractFactory` and `Contract` instances are connected to the first signer. So the account in the `owner` variable executed the deployment and `balanceOf` should return the enture supply amount.
+
+```
+expect(await hardhatToken.totalSupply()).to.equal(ownerBalance)
+```
+
+Again use the `Contract` instance to call a smart contract function. `totalSupply()` returns the token's supply amount - check that it's equal to the `ownerBalance`.
+Use `Chai` asserting functions called "matchers"
+
+If you need to test by sending a tx from a different account (or signer), can use the `connect()` method on the ethers.js `Contract` object to connect to a different account.
+
+```
+await hardhatToken.connect(addr1).transfer(addr2.address, 50)
+```
--- test/Token.js
+++ test/Token.js
@@ -1,15 +1,44 @@
+const { ethers } = require("hardhat") // Note - ethers variable in global scope
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers")
 const { expect } = require("chai")
 
 describe("Token contract", function () {
-  it("Deployment should assign the total supply of tokens to the owner", async function () {
-    const [owner] = await ethers.getSigners()
-
+  async function deployTokenFixture() {
+    const [owner, addr1, addr2] = await ethers.getSigners()
     const Token = await ethers.getContractFactory("Token")
 
     const hardhatToken = await Token.deploy()
+    await hardhatToken.deployed()
+
+    // Fixtures can return anything you consider useful for your tests
+    return { Token, hardhatToken, owner, addr1, addr2 }
+  }
+
+  it("Deployment should assign the total supply of tokens to the owner", async function () {
+    // const [owner] = await ethers.getSigners()
+    // const Token = await ethers.getContractFactory("Token")
+    // const hardhatToken = await Token.deploy()
+    const { hardhatToken, owner } = await loadFixture(deployTokenFixture)
 
     const ownerBalance = await hardhatToken.balanceOf(owner.address)
-
     expect(await hardhatToken.totalSupply()).to.equal(ownerBalance)
+  })
+
+  it("Should transfer tokens between accounts", async function () {
+    // const [owner, addr1, addr2] = await ethers.getSigners()
+    // const Token = await ethers.getContractFactory("Token")
+    // const hardhatToken = await Token.deploy()
+    const { hardhatToken, owner, addr1, addr2 } = await loadFixture(
+      deployTokenFixture
+    )
+
+    // Transfer 50 tokens from owner to addr1
+    await hardhatToken.transfer(addr1.address, 50)
+    expect(await hardhatToken.balanceOf(addr1.address)).to.equal(50)
+
+    // Transfer 50 tokens from addr1 to addr2
+    // Use 'connect(signer) to send a tx from another account
+    await hardhatToken.connect(addr1).transfer(addr2.address, 50)
+    expect(await hardhatToken.balanceOf(addr2.address)).to.equal(50)
   })
 })
